56553cfc7e708385fb1c50788fc86d9ed891883d
UreshaSew <vpusewwandi@gmail.com>
2020-11-11 05:29:55 +0530

    DataProvider Attribute

---
+++ src/test/java/LogIn.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LogIn {
+    @Test(dataProvider = "LoginData")
+public void LogIn1(String email,String password, boolean success){
+
+    System.out.println("Login credentials: "+"\n" +
+     "Email: "+ email+"\n" +
+            "Password: " + password +"\n" +
+            "Successful login: " + success + "\n" );
+}
+@DataProvider
+public Object [] [] LoginData (){
+    Object [][] data = new Object [3][3];
+    data [0][0] =" TestNG@Framework.com";
+    data [0][1] = "TestNG1234";
+    data [0][2] = true;
+    data[1][0] ="Joe@Doe.com";
+    data[1][1] = "DoeDoe34";
+    data [1][2]= false;
+    data[2][0] ="Test@AutomationU.com";
+    data[2][1]= "TAU1234" ;
+    data[2][2]=true;
+
+            return data;
+}
+
+}
